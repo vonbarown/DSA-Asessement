802f172547d78a25528874e7f8a78da62eba928b
vonbarown <vonielbrown@pursuit.org>
2020-02-16 13:04:20 -0500

    answer to recursive solution

--- 6.recursiveSum.js
+++ 6.recursiveSum.js
@@ -4,24 +4,17 @@
 // Your solution should be **recursive**. Your function must be pure (cannot use global variables)
 
 
-const recursiveSum = (arr, i = 0) => {
-    if (i >= arr.length) {
+const recursiveSum = (arr, i = 0, sum = 0) => {
+    if (i === arr.length + 1) {
         return
     }
-    let sum = 0
+
     // sum += arr[i]
+    console.log('arr', sum);
 
-    // sum += sum
-    // console.log(sum);
-    while (i < arr.length) {
-        sum += arr[i]
-    }
-
-    // console.log(sum);
-
-    recursiveSum(arr, i + 1)
+    recursiveSum(arr, i + 1, sum += arr[i])
 }
 
-const arr1 = [5, 2, 9, 11]
+const arr1 = [5, 2, 9, 11, 1]
 recursiveSum(arr1);
  // should return 27

82195cdc932a8f74e50be4b02779db0f0f1798b2
vonbarown <vonielbrown@pursuit.org>
2020-02-16 12:40:32 -0500

    more test cases to linkedlist

--- 1.traverseLinkedList.js
+++ 1.traverseLinkedList.js
@@ -14,12 +14,18 @@ let n2 = new Node(41)
 let n3 = new Node(33)
 let n4 = new Node(150)
 let n5 = new Node(11)
+let n6 = new Node(15)
+let n7 = new Node(8)
+let n8 = new Node(245)
 
 // Connect nodes, in turn creating the list
 n1.next = n2
 n2.next = n3
 n3.next = n4
 n4.next = n5
+n5.next = n6
+n6.next = n7
+n7.next = n8
 
 // Implement your code to traverse
 const traverse = (node) => {

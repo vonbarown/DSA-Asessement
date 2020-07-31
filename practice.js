13d4c30007593a85697fb8780b4206c3b93ddc75
Voniel B. Brown <52142269+vonbarown@users.noreply.github.com>
2020-07-30 20:06:23 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -80,23 +80,6 @@ let sample = [
 ];
 console.log(hourglassSum(sample));
 
-const diagonalDifference = (arr) => {
-  if (!arr.length) return 0;
-
-  let diagonal1 = 0,
-    diagonal2 = 0;
-
-  for (let row = 0; row < arr.length; row++) {
-    diagonal1 += arr[row][row];
-    // console.log(diagonal1)
-    diagonal2 += arr[row][arr.length - row - 1];
-  }
-
-  return Math.abs(diagonal1 - diagonal2);
-};
-
-console.log("dig", diagonalDifference(sample));
-
 //using a set to remove duplicates
 let arr1 = [2, 2, 1, 3, 4];
 let arr2 = [2, 4, , 3, 6];
